89c15f66d7625eb5ea60c78eb1ec7dd0b0be9df7
Akintomiwa Opemipo <ibnakintomiwa@gmail.com>
2026-05-03 12:19:25 +0000

    Added broker services

---
+++ services/broker.py
@@ -0,0 +1,35 @@
+from typing import Any
+import httpx
+
+BASE_URL = "http://192.168.103.101:51081/v3/api/v3"
+
+async def submit(payload: dict[str, Any]):
+    async with httpx.AsyncClient() as client:
+        res = await client.post(f"{BASE_URL}/brokerSubmission", json=payload)
+        return res.json()
+
+
+async def get_submission(case_file_version_id: str):
+    async with httpx.AsyncClient() as client:
+        res = await client.get(
+            f"{BASE_URL}/submission",
+            params={"caseFileVersionId": case_file_version_id, "userTimeZoneOffset": -5}
+        )
+        return res.json()
+
+
+async def decline(case_file_version_id: str):
+    async with httpx.AsyncClient() as client:
+        res = await client.post(
+            f"{BASE_URL}/declineToQuote",
+            json={
+                "caseFileVersionId": case_file_version_id,
+                "userTimeZoneOffset": -5,
+                "declineToQuote": {
+                    "declinationReason": "NONE",
+                    "declinationDescription": "Auto-declined",
+                    "stateProvince": "FL"
+                }
+            }
+        )
+        return res.json()
